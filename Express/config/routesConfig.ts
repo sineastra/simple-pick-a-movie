f2971fda30e6009b3f724530dc2457ffc3f4b49d
Sineastra <lord.of.light.0002@gmail.com>
2022-02-14 13:56:01 +0200

    added base /api message to express

--- Express/config/routesConfig.ts
+++ Express/config/routesConfig.ts
@@ -1,11 +1,15 @@
 import { Express } from "express"
 import userController from "../controllers/userController"
 import moviesController from "../controllers/moviesController"
+import { arch } from "os"
 
 
 const routesConfig = (app: Express) => {
 	app.use("/api/user", userController)
 	app.use("/api/movies", moviesController)
+	app.use("/api", (req, res) => {
+		res.send("api working")
+	})
 }
 
 export default routesConfig
